c478532f04c4c2fc069850dffb0e27103fc10697
gabrieldavid <gabrieldavid75315@gmail.com>
2022-06-05 13:45:47 -0300

    push

--- server.ts
+++ server.ts
@@ -1,5 +1,7 @@
+import http from "http";
 import { app } from "./app";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log("Server is running on 3000!"));
+const server = http.createServer(app);
+server.listen(PORT);
