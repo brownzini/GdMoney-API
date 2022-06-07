a818d333a4a6cf077ff4d272c973ce8d830ec2ae
gabrieldavid <gabrieldavid75315@gmail.com>
2022-06-07 17:26:49 -0300

    up

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+	apiKey: "AIzaSyDFrU4heJYZhD04F031lAnCZHueFcGsTkg",
+	authDomain: "projeto-gdmoney.firebaseapp.com",
+	projectId: "projeto-gdmoney",
     storageBucket: "projeto-gdmoney.appspot.com",
     messagingSenderId: "317925113974",
     appId: "1:317925113974:web:d65bcdc4f4d77ed48f1f57"
