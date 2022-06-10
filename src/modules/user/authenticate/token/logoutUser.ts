1545ce80d03114e495a826388ee86d5121a1ca3a
gabrieldavid <gabrieldavid75315@gmail.com>
2022-06-10 17:39:31 -0300

    renamed

--- src/modules/user/authenticate/token/logoutUser.ts
+++ src/modules/user/authenticate/token/logoutUser.ts
@@ -27,7 +27,7 @@ export default async function logoutUser(request: Request, response: Response) {
             refresh_token: ""
          });
         updateUser(data);
-        return response.status(200).json('Updated');
+        return response.status(200).json('Success');
     } catch(err) {
         return response.status(401).json({
             message: "Token is invalid or expired"
