5a816af14a3109e599b29a29ea06f17ee6a0f611
gabrieldavid <gabrieldavid75315@gmail.com>
2022-06-12 20:07:47 -0300

    to cors problem

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { router } from "./shared/infra/http/routes";
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
