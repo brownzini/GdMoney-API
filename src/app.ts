4a49d57795fdfdc5b4bad50e154dce732432665b
gabrieldavid <gabrieldavid75315@gmail.com>
2022-06-12 20:16:32 -0300

    to cors problem

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://gd-money.vercel.app"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "*");
   next();
 });
 
