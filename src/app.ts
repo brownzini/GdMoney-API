1366a2678b53d5bca8a5cbbb47e751dc68106738
gabrieldavid <gabrieldavid75315@gmail.com>
2022-06-12 20:04:48 -0300

    up changes

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,16 @@ import "express-async-errors";
 import { AppError } from "./shared/errors/AppError";
 import { router } from "./shared/infra/http/routes";
 
-const cors = require("cors");
-
 const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.json());
 app.use(router);
-app.use(cors())
 
 app.use(
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
