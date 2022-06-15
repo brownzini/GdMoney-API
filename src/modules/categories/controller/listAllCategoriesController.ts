f28772c2f6608871362ff337c43234e95411a04f
gabrieldavid <gabrieldavid75315@gmail.com>
2022-06-15 13:40:38 -0300

    up to deploy

--- src/modules/categories/controller/listAllCategoriesController.ts
+++ src/modules/categories/controller/listAllCategoriesController.ts
@@ -4,7 +4,7 @@ import { getAllCategories } from '../../../firebase/services/Categories';
 const listAllCategoriesController = async(req: Request, res: Response): Promise<Response> => {
     try { 
         await getAllCategories().then(resp => {
-          return res.status(200).json({ categories: resp }).send();
+          return res.status(200).json({ categories: resp });
         });
       } catch (err) {
           return res.status(404).json("Error in getting all categories");
