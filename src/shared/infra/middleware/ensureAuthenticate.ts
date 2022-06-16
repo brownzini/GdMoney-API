94745e35b84e7a26bee9ec239aec6610b9d3e30f
gabrieldavid <gabrieldavid75315@gmail.com>
2022-06-16 00:11:20 -0300

    new refresh token

--- src/shared/infra/middleware/ensureAuthenticate.ts
+++ src/shared/infra/middleware/ensureAuthenticate.ts
@@ -2,6 +2,9 @@ import { verify } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 import auth from "../../../config/auth";
 import { getUserProfile } from "../../../firebase/services/Users";
+import { getRefreshToken, updateRefreshToken } from "../../../firebase/services/Authenticate";
+import generateToken from "../../../provider/generateToken";
+import dayjs from "dayjs";
 
 export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
     const authToken = request.headers.authorization
@@ -17,8 +20,26 @@ export async function ensureAuthenticated(request: Request, response: Response, 
     try {
         const { sub: user_id } = verify(token, auth.secret_token);
         const user = await getUserProfile(user_id+'');
+
+        const id_refresh_token = user.refresh_token;
+        const rt = await getRefreshToken(id_refresh_token);
+    
+        if (rt === null) {
+            return response.status(401).json({
+                message: "Invalid token"
+            });
+        } 
+    
+        await generateToken(rt.user_id);
+        const refreshTokenExpired = dayjs().isAfter(dayjs.unix(rt.expiresIn));
+    
+        if (refreshTokenExpired) {
+            const expiresIn = dayjs().add(30, "minutes").unix();
+            updateRefreshToken(expiresIn, id_refresh_token);
+        }
+
         if( user.islogged ) {
-           next();
+            next();
         } else {
            return response.status(401).json({
               message: "You aren't logged"
