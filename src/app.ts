f0e5cf4d94aa5cb5bb5a6ec2ede0a3a0fc2f4c30
gabrieldavid <gabrieldavid75315@gmail.com>
2022-06-15 23:50:58 -0300

    modify permissions

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,9 @@ import { router } from "./shared/infra/http/routes";
 const app = express();
 //
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "*");
+  res.header("ACESS-CONTROL-ALLOW-METHODS", "GET, POST, PUT, DELETE");
   next();
 });
 
