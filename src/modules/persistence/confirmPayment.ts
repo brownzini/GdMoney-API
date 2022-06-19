90ad816a049751c3d7d084be7b772a11f9f8f7b3
gabrieldavid <gabrieldavid75315@gmail.com>
2022-06-18 23:46:07 -0300

    up to deploy

--- src/modules/persistence/confirmPayment.ts
+++ src/modules/persistence/confirmPayment.ts
@@ -8,30 +8,34 @@ const confirmPayment = (req:Request, res:Response) => {
     const currentData = dayjs().format('YYYY-MM-DD HH:mm:ss');
     let count = false;
 
-    products.forEach(product => { 
-        addTransaction({
-            product_address: product.product_address,
-            product_name: product.product_name,
-            user_id: product.user_id,
-            data: currentData,
-            status:"success",
-        }).catch(err => { 
-            count = true;
+    try {
+        products.forEach(product => { 
+            addTransaction({
+                product_address: product.product_address,
+                product_name: product.product_name,
+                user_id: product.user_id,
+                data: currentData,
+                status:"success",
+            }).catch(err => { 
+                count = true;
+            });
+            const data:any = {
+                id: product.product_address,
+                forSale: false,
+                user_id: product.user_id,
+            };
+            updateProduct(data);
         });
-        const data:any = {
-            id: product.product_address,
-            forSale: false,
-            user_id: product.user_id,
-        };
-        updateProduct(data);
-    });
-
-    setTimeout(() => { 
-        if (count === true) { 
-            return res.status(400).json({message:'Error in operation'});
-        }
-        return res.status(200).json({message:"Success in operation"});
-    },3000)
+    
+        setTimeout(() => { 
+            if (count === true) { 
+                return res.status(400).json({message:'Error in operation'});
+            }
+            return res.status(200).json({message:"Success in operation"});
+        },3000)
+    } catch (error) {
+        return res.status(200).json({message:error.message});
+    }
 }
 
 export default confirmPayment;
