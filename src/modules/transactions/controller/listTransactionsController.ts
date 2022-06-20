097ab6e1657dd475faed22ff6bb3dba96adcab42
gabrieldavid <gabrieldavid75315@gmail.com>
2022-06-20 15:40:01 -0300

    apply transaction route

--- src/modules/transactions/controller/listTransactionsController.ts
+++ src/modules/transactions/controller/listTransactionsController.ts
@@ -5,7 +5,7 @@ const listTransactionsController = async(req: Request, res: Response): Promise<R
     const id = req.params.id;
     try { 
         await getAllTransactions(id).then(resp => { 
-          return res.status(200).json({ products: resp });
+          return res.status(200).json({ transactions: resp });
         }); 
     } catch (err) {
         return res.status(404).json({message:"Error in getting all products"});
